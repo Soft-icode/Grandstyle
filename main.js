e1e0b38322fb3d75d338b2434d7c63ba694ccbcd
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-11-27 20:28:18 +0100

    Added a responsive mobile menu

--- main.js
+++ main.js
@@ -20,3 +20,16 @@ document.addEventListener('scroll', () => {
     navigationBar.classList.remove('scrolled');
   }
 });
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('#nav');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+  })
+);
