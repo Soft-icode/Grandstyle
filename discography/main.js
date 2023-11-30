b0643c4a7321f2560d2c1ebc2263058e69b3c6e9
Joseph Oritseweyinmi <josephoritseweyinmi5@gmail.com>
2023-12-01 00:34:48 +0100

    Added the discography page

---
+++ discography/main.js
@@ -0,0 +1,22 @@
+document.addEventListener('scroll', () => {
+  const navigationBar = document.querySelector('#nav');
+
+  if (window.scrollY > 0) {
+    navigationBar.classList.add('scrolled');
+  } else {
+    navigationBar.classList.remove('scrolled');
+  }
+});
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('#nav');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  nav.classList.toggle('active');
+});
+document.querySelectorAll('.nav-link').forEach((n) =>
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    nav.classList.remove('active');
+  })
+);
